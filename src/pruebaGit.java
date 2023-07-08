ef3eea0f027224a71616418434dff63262178c8b
Martin <Martin@DESKTOP-GKJJHE3.mshome.net>
2023-07-08 00:42:03 -0300

    segundo commit

--- src/pruebaGit.java
+++ src/pruebaGit.java
@@ -4,6 +4,8 @@ public class pruebaGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("proyecto de pruebas");
+		String nombre = "Juan perez";
+
 	}
 
 }
